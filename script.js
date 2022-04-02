8b80b0e6559568573e7500633f5427482e41a20c
msladjana <msladjana@users.noreply.github.com>
2022-04-02 23:28:15 +0200

    modify back to top button

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-//JS for back-to-top button
 //Get the button
-let mybutton = document.getElementById('back-to-top');
+const mybutton = document.getElementById('back-to-top-btn');
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
-	scrollFunction()
+	scrollFunction();
 };
 
 function scrollFunction() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
 		mybutton.style.display = 'block';
 	} else {
 		mybutton.style.display = 'none';
 	}
 }
 // When the user clicks on the button, scroll to the top of the document
-// mybutton.addEventListener('click', backToTop);
 
+mybutton.addEventListener('click', backToTop);
 function backToTop() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
